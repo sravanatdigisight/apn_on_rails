9730b5e6ada30d4ea7b005a1444fd5999b849326
niku <niku@niku.name>
2012-04-16 14:02:18 +0900

    add be_same_meaning_as matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,3 +66,9 @@ end
 
 class BlockRan < StandardError
 end
+
+RSpec::Matchers.define :be_same_meaning_as do |expected|
+  match do |actual|
+    ActiveSupport::JSON.decode(actual) == ActiveSupport::JSON.decode(expected)
+  end
+end
