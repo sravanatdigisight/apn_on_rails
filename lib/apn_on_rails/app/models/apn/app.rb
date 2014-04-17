a7de2d671a351155897aa48345292c8a715e9059
Douglas Merrell <doug@digisight.net>
2014-04-17 12:27:54 -0700

    Added debugging output

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -131,6 +131,7 @@ class APN::App < APN::Base
   # This can be run from the following Rake task:
   #   $ rake apn:feedback:process
   def process_devices
+    puts "Enter process_devices"
     if self.cert.nil?
       raise APN::Errors::MissingCertificateError.new
       return
@@ -139,12 +140,14 @@ class APN::App < APN::Base
   end # process_devices
 
   def self.process_devices
+    puts "Enter self.process_devices, cert = #{self.cert.inspect}"
     apps = APN::App.all
     apps.each do |app|
       app.process_devices
     end
     if !configatron.apn.cert.blank?
       global_cert = File.read(configatron.apn.cert)
+      puts "[1;31mcert: #{configatron.apn.cert}[0m"
       APN::App.process_devices_for_cert(global_cert)
     end
   end
@@ -152,6 +155,7 @@ class APN::App < APN::Base
   def self.process_devices_for_cert(the_cert)
     puts "in APN::App.process_devices_for_cert"
     APN::Feedback.devices(the_cert).each do |device|
+      puts "[1;31mdevice: #{device.inspect}[0m"
       if device.last_registered_at < device.feedback_at
         puts "device #{device.id} -> #{device.last_registered_at} < #{device.feedback_at}"
         device.destroy
